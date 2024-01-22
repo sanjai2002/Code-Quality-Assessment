64dc09d110e6bc65930d751f9b908dcdf5116507
SANJAI H <84699125+sanjai2002@users.noreply.github.com>
2024-01-22 11:03:12 +0530

    add the name variable

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using CreateApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+String Name;
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
